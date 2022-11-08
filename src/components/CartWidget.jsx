d85ed4bb7c562c15f3a094d93456e66aabdc0ff0
sebaroi <sroizenberg@gmail.com>
2022-11-08 15:59:26 -0300

    otra vez

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
-import carrito from './styles/carrito.css';
+import './styles/carrito.css';
 
 
 const CartWidget = () => {
     return (
         <div>
 
-            <button className="carrito btn btn-secondary my-2 my-sm-0" type="submit">
-            <FontAwesomeIcon icon={faCartShopping} />
-
-                 
-            </button>
+            <Button variant="primary" className='carrito'>
+            <FontAwesomeIcon icon={faCartShopping} />    
+            </Button>{' '} 
         </div>
     );
 }
--- src/components/FormSearch.jsx
+++ src/components/FormSearch.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+
 
 const FormSearch = ({busqueda, TextoBoton}) => {
     return (
         <div>
           <form className="d-flex">
             <input className="form-control me-sm-2" type="text" placeholder={busqueda} />
-            <button className="btn btn-secondary my-2 my-sm-0" type="submit">{TextoBoton}</button>
+            <Button variant="primary" >{TextoBoton}</Button>{' '}
           </form>
         </div>
     );
