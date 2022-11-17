d164934d5139968e64488cb9f578dddb90c2e2e8
sebasroi <sroizenberg@gmail.com>
2022-11-17 12:55:28 -0300

    productos

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import NavBar from './NatBar';
+import productos from './Products';
+
+
 
 
 
 const ItemListContainer = () => {
+
+    const productList = new Promise((resolve) => setTimeout(() => {resolve(productos);}, 3000));
+
+    productList.then((data) => console.log({data}));
+
     return (
         <div>
             <NavBar greeting={"Bienvenidos"}/>
---
+++ src/components/Products.jsx
@@ -0,0 +1,34 @@
+
+
+  const productos = [
+    {
+      id:"1",
+      name: "Placa de video",
+      description: "Esta buenisima",
+/*       imageProd: require("./imagenes/placaVideo.webp"), */
+      price: "34.660",
+      stock: 10,
+    },
+    {
+      id:"2",
+      name: "Mouse",
+      description: "Inalambrico esta buenisima",
+   /*    imageProd: require("./imagenes/mouse1.webp"), */
+      price: "3500",
+      stock: 5,
+    },
+    {
+      id:"3",
+      name: "silla gammer",
+      description: "que silla Esta buenisima",
+/*       imageProd: require("./imagenes/ofertasilla.webp"), */
+      price: "21500",
+      stock: 3,
+    },
+]
+
+
+
+export default productos;
+
+
